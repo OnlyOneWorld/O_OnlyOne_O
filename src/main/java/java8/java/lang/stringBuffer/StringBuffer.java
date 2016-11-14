e09c02da5ba0fe765c11b6e19ba6af7c7a3caaf9
wylgithub <ltwuyanlong@sina.com>
2016-11-14 22:19:15 +0800

    修改后提交

--- src/main/java/java8/java/lang/stringBuffer/StringBuffer.java
+++
@@ -1,9 +0,0 @@
-package java8.java.lang.stringBuffer;
-/** 
- * @author  Administrator: 
- * @version 创建时间：2016-9-16 下午11:18:07 
- * 类说明 
- */
-public class StringBuffer {
-
-}
--- src/main/java/java8/java/lang/stringBuilder/StringBuilder.java
+++
@@ -1,9 +0,0 @@
-package java8.java.lang.stringBuilder;
-/** 
- * @author  Administrator: 
- * @version 创建时间：2016-9-16 下午11:17:55 
- * 类说明 
- */
-public class StringBuilder {
-
-}
